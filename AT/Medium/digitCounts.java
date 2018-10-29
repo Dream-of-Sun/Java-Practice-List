93ca9129d972d13e7e56e89416605f337fc0e4a3
sun <850464756@qq.com>
2018-10-29 22:26:32 +0800

    AT

---
+++ AT/Medium/digitCounts.java
@@ -0,0 +1,12 @@
+package Medium;
+
+import java.util.ArrayList;
+
+public class digitCounts {
+	public void dC(int k, int n) {
+		ArrayList list = new ArrayList();
+		for(int n=0;;++n;)
+		list.add(i);
+        // write your code here
+    }
+}
